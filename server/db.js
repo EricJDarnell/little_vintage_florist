8caa9abedaaaa877defc7321f1293adc91d67d3d
EricJDarnell <eric.j.darnell@gmail.com>
2025-04-13 20:24:02 -0400

    gallery data seeded

--- server/db.js
+++ server/db.js
@@ -15,6 +15,7 @@ const JWT = process.env.JWT || 'shhh';
 const createTables = async()=> {
     await client.query(`drop table if exists products`);
     await client.query(`drop table if exists gallery`);
+    await client.query(`drop table if exists categories`);
     await client.query(`drop table if exists users`);
     await client.query(`create table users(
           id uuid primary key,
@@ -25,27 +26,39 @@ const createTables = async()=> {
           created_at timestamp default now(),
           updated_at timestamp default now()
         )`);
-    await client.query(`create table categories (
-          id uuid primary key,
-          title varchar(50)
-        )`);
     await client.query(`create table products (
           id uuid primary key,
           name varchar(100) not null,
           price decimal(10, 2) not null,
           description text,
-          category uuid references categories(id) not null,
+          category varchar(50),
           image_url text
         )`);
     await client.query(`create table gallery (
           id uuid primary key,
           title varchar(100),
-          imagew_url text not null,
-          category uuid references categories(id) not null
+          image_url text not null,
+          category varchar(50)
         )`);
+};
+
+const addToGallery = async({ title, image_url, category }) => {
+    const response = await client.query(`
+          insert into gallery(id, title, image_url, category)
+          values($1, $2, $3, $4) returning *
+        `,
+    [uuid.v4(), title, image_url, category]);
+    return response.rows[0];
+};
+
+const fetchGallery = async() => {
+    const response = await client.query(`select * from gallery`);
+    return response.rows;
 };
 
 module.exports = {
     client,
-    createTables
+    createTables,
+    addToGallery,
+    fetchGallery
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const {
     client,
-    createTables
+    createTables,
+    addToGallery,
+    fetchGallery
 } = require("./db");
 
+const cloudinary = require('cloudinary').v2;
 const express = require("express");
 const { JsonWebTokenError } = require("jsonwebtoken");
 const app = express();
@@ -19,6 +22,19 @@ app.use(
     express.static(path.join(__dirname, "../client/dist/assets"))
 );
 
+cloudinary.config({
+    cloud_name: 'dekkpjwfz',
+    secure: true
+});
+
+app.get("/api/gallery", async (req, res, next) => {
+    try {
+        res.send(await fetchGallery());
+    } catch (err) {
+        next(err);
+    }
+});
+
 const init = async () => {
     const port = process.env.PORT || 3000;
     await client.connect();
@@ -26,5 +42,341 @@ const init = async () => {
 
     await createTables();
     console.log("tables created");
+
+    const [ pic01, pic02, pic03, pic04, pic05, pic06, pic07, pic08, pic09, pic10, pic11, pic12, pic13, pic14, pic15, pic16, pic17, pic18, pic19, pic20, pic21, pic22, pic23, pic24, pic25, pic26, pic27, pic28, pic29, pic30 ] = await Promise.all([
+        addToGallery({
+            title: '001',
+            image_url: cloudinary.url('PXL_20250411_012037333.PORTRAIT.ORIGINAL_2_dtaoic', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '002',
+            image_url: cloudinary.url('PXL_20250411_005148844.PORTRAIT.ORIGINAL_2_s8apiy', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '003',
+            image_url: cloudinary.url('PXL_20250411_002939534.PORTRAIT_2_gwdnmf', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '004',
+            image_url: cloudinary.url('PXL_20250411_002956356.PORTRAIT.ORIGINAL_3_fmsvsj', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '005',
+            image_url: cloudinary.url('PXL_20250411_000945524.PORTRAIT.ORIGINAL_3_p5hpsc', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '006',
+            image_url: cloudinary.url('PXL_20250411_000922138.PORTRAIT.ORIGINAL_2_eymwli', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '007',
+            image_url: cloudinary.url('PXL_20250408_201540273.PORTRAIT_apq8xk', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '008',
+            image_url: cloudinary.url('PXL_20250405_172143208.PORTRAIT_3_qmkcpc', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '009',
+            image_url: cloudinary.url('PXL_20250408_201433984.PORTRAIT.ORIGINAL_2_yndhad', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '010',
+            image_url: cloudinary.url('PXL_20250408_201359528.PORTRAIT_jbdszg', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '011',
+            image_url: cloudinary.url('PXL_20250405_175652037.PORTRAIT_2_lpvcch', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '012',
+            image_url: cloudinary.url('PXL_20250405_175546285.PORTRAIT_2_sogf6p', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '013',
+            image_url: cloudinary.url('PXL_20250405_171728233.PORTRAIT_2_aojjl6', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '014',
+            image_url: cloudinary.url('PXL_20250405_011523554.PORTRAIT_lyizzn', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '015',
+            image_url: cloudinary.url('PXL_20250405_010710202.PORTRAIT.ORIGINAL_3_taoeay', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '016',
+            image_url: cloudinary.url('PXL_20250405_011501450.RAW-01.COVER_kejrwf', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '017',
+            image_url: cloudinary.url('PXL_20250405_003732057.PORTRAIT.ORIGINAL_3_ajs4oe', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '018',
+            image_url: cloudinary.url('PXL_20250405_001416989.PORTRAIT_2_ntyige', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '019',
+            image_url: cloudinary.url('PXL_20250313_202208170.RAW-01.COVER_jegoa1', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '020',
+            image_url: cloudinary.url('PXL_20250313_173440376.PORTRAIT_mscwjt', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '021',
+            image_url: cloudinary.url('PXL_20250312_200550746.RAW-01.COVER_iafapo', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '022',
+            image_url: cloudinary.url('PXL_20250312_011017768.PORTRAIT_2_euczfj', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '023',
+            image_url: cloudinary.url('PXL_20250312_014354130.PORTRAIT_zyoebs', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '024',
+            image_url: cloudinary.url('PXL_20250312_180956923.RAW-01.COVER_apw9dl', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '025',
+            image_url: cloudinary.url('PXL_20250312_011914167.PORTRAIT_fnxup1', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '026',
+            image_url: cloudinary.url('PXL_20250312_014154326.RAW-01.COVER_fajglb', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '027',
+            image_url: cloudinary.url('PXL_20250311_200452636.PORTRAIT.ORIGINAL_dvrfyl', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '028',
+            image_url: cloudinary.url('PXL_20250310_045439356.PORTRAIT_plydrl', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '029',
+            image_url: cloudinary.url('PXL_20250310_045739456.PORTRAIT_2_bycbdm', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        }),
+        addToGallery({
+            title: '030',
+            image_url: cloudinary.url('PXL_20250310_045209579.PORTRAIT.ORIGINAL_v2fn14', {
+                transformation: [
+                    {
+                        fetch_format: 'auto'
+                    }
+                ]
+                        }),
+            category: 'arrangement'
+        })
+    ]);
+    console.log(`gallery seeded`);
+    app.listen(port, () => console.log(`Server running on port ${port}`));
 };
+
 init();
